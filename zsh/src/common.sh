7209c442348b3877b795edc387b5b329524d2d70
Kenta Sato <k-sato@mfac.jp>
2012-10-14 22:24:33 +0900

    more useful feature for emacsclient.

--- zsh/src/common.sh
+++ zsh/src/common.sh
@@ -111,6 +111,30 @@ if type ggrep > /dev/null 2>&1; then
     alias grep=ggrep
 fi
 
+function emacs_server_start {
+    if ps x | grep emacs | grep '\--daemon' > /dev/null 2>&1; then
+        echo "[INFO] lived 'emacs --daemon'"
+    else
+        echo "[INFO] start 'emacs --daemon'"
+        env emacs --daemon
+    fi
+}
+
+function emacs_server_stop {
+    if ps x | grep emacs | grep '\--daemon' > /dev/null 2>&1; then
+        echo "[INFO] stop  'emacs --daemon'"
+        emacsclient -e '(server-force-delete)'
+        ps x | grep emacs | grep '\--daemon' | awk '{print $1}' | xargs kill
+    else
+        echo "[INFO] not lived 'emacs --daemon'";
+    fi
+}
+
+function emacs_server_restart {
+    emacs_server_stop;
+    emacs_server_start;
+}
+
 ## emacsを使う。(emacsclient)
 export EDITOR="emacsclient -nw -c"
 export VISUAL="emacsclient -nw -c"
@@ -119,10 +143,5 @@ if ! type emacs > /dev/null 2>&1; then
     alias emacs="vim"
 else
     ## emacs
-    if ps x | grep emacs | grep '\--daemon' > /dev/null 2>&1; then
-        echo "[INFO] lived 'emacs --daemon'"
-    else
-        echo "[INFO] start 'emacs --daemon'"
-        env emacs --daemon
-    fi
+    emacs_server_start;
 fi

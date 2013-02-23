265326330aa13b653ad5a93553787e9682653423
karupanerura <karupa@cpan.org>
2013-02-23 13:29:45 +0900

    no required shellenv.el

--- zsh/src/function.sh
+++ zsh/src/function.sh
@@ -3,12 +3,8 @@ function perlv {
 }
 
 function emacs_server_start {
-    ## create emacs env file
-    env | awk -F'=' '$1 ~ /^(PATH|PERL5LIB)$/ { print "(setenv \"" $1 "\" \"" $2 "\")" }' > ~/.emacs.d/shellenv.el
-
     if ps x | grep emacs | grep '\--daemon' > /dev/null 2>&1; then
         echo "[INFO] lived 'emacs --daemon'"
-        emacsclient -e '(load-shellenv)'
     else
         echo "[INFO] start 'emacs --daemon'"
         env emacs --daemon

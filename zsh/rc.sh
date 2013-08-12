47ffd0144cbe56dd90bcb87787c283ddb4f65840
karupanerura <karupa@cpan.org>
2013-08-12 19:25:01 +0900

    finalize after load local setting.

--- zsh/rc.sh
+++ zsh/rc.sh
@@ -13,8 +13,9 @@ source $ZSHRC_BASEDIR/src/alias.sh
 source $ZSHRC_BASEDIR/src/completion.sh
 source $ZSHRC_BASEDIR/src/prompt.sh
 source $ZSHRC_BASEDIR/src/misc.sh
-source $ZSHRC_BASEDIR/src/finalize.sh
 
 if [ -f ~/.zshrc.local ]; then
     source ~/.zshrc.local
 fi
+
+source $ZSHRC_BASEDIR/src/finalize.sh

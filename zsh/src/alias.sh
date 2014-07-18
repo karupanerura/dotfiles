013d45f9cfd7d73bf965de2e93366e2df876795f
karupanerura <karupa@cpan.org>
2014-07-18 17:02:04 +0900

    peco is very useful

--- zsh/src/alias.sh
+++ zsh/src/alias.sh
@@ -27,7 +27,7 @@ fi
 alias tmux="tmux -2 -L karupas_dev"
 alias scp="scp -C"
 alias random-string='perl -MString::Random=random_string -E "say+random_string(q{s} x (\$ARGV[0] || 10))"'
-alias webalize='perl -MTest::TCP -E "exec(qq{plackup -o=localhost -p=@{[ empty_port() ]} -MPlack::App::Directory -e \"Plack::App::Directory->new(root => q{.})->to_app\"})"'
+alias webalize='perl -MTest::TCP -E "exec(qq{plackup -p=@{[ empty_port() ]} -MPlack::App::Directory -e \"Plack::App::Directory->new(root => q{.})->to_app\"})"'
 alias sync='sync && sync && sync'
 alias shutdown='sync && shutdown'
 alias reboot='sync && reboot'
@@ -36,3 +36,6 @@ alias rot13='tr a-mn-zA-MN-Z0-45-9 n-za-mA-MN-Z5-90-4'
 alias uri-escape="perl -MURI::Escape=uri_escape -pe '\$_ = uri_escape \$_'"
 alias uri-unescape="perl -MURI::Escape=uri_unescape -pe '\$_ = uri_unescape \$_'"
 alias whitespace-fix="perl -i -pe 's/\s+$/\n/'"
+alias perldoc-peco='perldoc `perl-module-list | peco`'
+alias cpandoc-peco='cpandoc `perl-module-list | peco`'
+alias cpanm-peco='cpanm `perl-module-list | peco`'

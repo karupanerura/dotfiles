f44b7265ba10741c6378d7af555eccb1523a4a2b
karupanerura <karupa@cpan.org>
2015-01-20 18:29:02 +0900

    updated dotfiles

--- zsh/src/alias.sh
+++ zsh/src/alias.sh
@@ -27,12 +27,12 @@ fi
 alias tmux="tmux -2 -L karupas_dev"
 alias scp="scp -C"
 alias gi="git"
-alias sudo="sudo -H"
+alias sudo="sudo -H -i"
 alias random-string='perl -MString::Random=random_string -E '"'"'say+random_string(q{s}x($ARGV[0]||10))'"'"''
-alias sync='sudo -H sync && sudo -H sync && sudo -H sync'
-alias shutdown='sudo -H sync && sudo -H shutdown'
-alias reboot='sudo -H sync && sudo -H reboot'
-alias halt='sudo -H sync && sudo -H halt'
+alias sync='sudo sync && sudo sync && sudo sync'
+alias shutdown='sudo sync && sudo shutdown'
+alias reboot='sudo sync && sudo reboot'
+alias halt='sudo sync && sudo halt'
 alias rot13='tr a-mn-zA-MN-Z0-45-9 n-za-mA-MN-Z5-90-4'
 alias strings='strings -a' # CVE-2014-8485
 alias uri-escape="perl -MURI::Escape=uri_escape -pe '\$_ = uri_escape \$_'"

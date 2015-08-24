4067c947d4c1a2c7f190965faf5bc6e5f9296910
karupanerura <karupa@cpan.org>
2015-08-24 12:02:41 +0900

    bye bye lv

--- zsh/src/common.sh
+++ zsh/src/common.sh
@@ -95,23 +95,6 @@ chpwd_functions=($chpwd_functions dirs)
 ## cdで移動してもpushdと同じようにディレクトリスタックに追加する。
 setopt auto_pushd
 
-if type lv > /dev/null 2>&1; then
-    ## lvを優先する。
-    export PAGER="lv"
-else
-    ## lvがなかったらlessを使う。
-    export PAGER="less"
-fi
-if [ "$PAGER" = "lv" ]; then
-    ## -c: ANSIエスケープシーケンスの色付けなどを有効にする。
-    ## -l: 1行が長くと折り返されていても1行として扱う。
-    ##     （コピーしたときに余計な改行を入れない。）
-    export LV="-c -l"
-else
-    ## lvがなくてもlvでページャーを起動する。
-    alias lv="$PAGER"
-fi
-
 ## GNU grepがあったら優先して使う。
 if type ggrep > /dev/null 2>&1; then
     alias grep=ggrep
